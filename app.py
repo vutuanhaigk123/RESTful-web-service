5e90e2fd0855889fb5a41f9b5a56b26ad02d8503
Anil Bandari <anilbandari.960@gmail.com>
2019-05-09 20:04:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,31 +3,31 @@ import json
 
 
 app = Flask(__name__)
-jData = json.loads(open('./restuarants.json').read())
-data=jData["Restuarants"]
+jData = json.loads(open('./restaurants.json').read())
+data=jData["Restaurants"]
 
 # Intial request Ex: http://192.168.99.100:5000/
 @app.route('/')
 def route_main():
     return "RESTful Webservice Started. Request data with proper URL"
 
-# Returns JSON which containes all restuarants
-@app.route('/getrestuarants/')
-def restuarants_all():
+# Returns JSON which containes all restaurants
+@app.route('/getrestaurants/')
+def restaurants_all():
     return render_template("index.html",items=data)
 
-# Returns restuarants JSON which matches the id
-@app.route('/getrestuarants/<string:id>/')
-def restuarants_by_id(id=''):
+# Returns restaurants JSON which matches the id
+@app.route('/getrestaurants/<string:id>/')
+def restaurants_by_id(id=''):
     myList=[]
     for element in data:
         if element["id"] == id:
             myList.append(element)
     return render_template("index.html",items=myList)
 
-# Returns the restuarants JSON with particualr food type
-@app.route('/getrestuarants/type/<string:type_of_food>/')
-def restuarants_by_type(type_of_food=''):
+# Returns the restaurants JSON with particualr food type
+@app.route('/getrestaurants/type/<string:type_of_food>/')
+def restaurants_by_type(type_of_food=''):
     myList=[]
     for element in data:
         if element["type_of_food"].lower() == type_of_food.lower():
